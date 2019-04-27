dec1d4a926390f6349ef876072b73d8a7e6b0f62
Ty3TECHLtd <you@example.com>
2019-04-27 07:02:10 -0400

    wrote data populating script

--- ProTwo/populate_data.py
+++ ProTwo/populate_data.py
@@ -1,24 +1,23 @@
+import django
 from faker import Faker
 from AppTwo.models import User
-import random
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppTwo.settings')
-
-import_django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProTwo.settings')
 django.setup()
-
 
 fakegen = Faker()
 
 
-def populate():
+def populate(N):
     for entry in range(N):
 
         fake_fname = fakegen.name()
         fake_lname = fakegen.name()
         fake_email = fakegen.email()
 
-        user = User.objects.get_or_create(first_name=fake_fname, last_name=fake_lname, email=fake_email)
+        user = User.objects.get_or_create(first_name=fake_fname,
+                                          last_name=fake_lname,
+                                          email=fake_email)
         user.save()
 
 
